39cbedda443340f3b6461107ff96508194fe61ea
root <root@ChrUbuntu.(none)>
2014-02-19 21:13:27 +1000

    what I did to make it work on my chrbuntu box

--- scripts/create.grub.key.sh
+++ scripts/create.grub.key.sh
@@ -9,18 +9,20 @@ command -v grub-install > /dev/null || { echo "## please install grub" ; exit 1 
 command -v tar > /dev/null || { echo "## please install tar" ; exit 1 ; }
 command -v curl > /dev/null || { echo "## please install curl" ; exit 1 ; }
 command -v whiptail >/dev/null 2>&1 || { echo "whiptail (pkg libnewt) required for this script" >&2 ; exit 1 ; }
-command -v sgdisk >/dev/null 2>&1 || { echo "sgdisk (pkg gptfdisk) required for this script" >&2 ; exit 1 ; }
+#command -v sgdisk >/dev/null 2>&1 || { echo "sgdisk (pkg gptfdisk) required for this script" >&2 ; exit 1 ; }
 
-disks=`sudo parted --list | awk -F ": |, |Disk | " '/Disk \// { print $2" "$3$4 }'`
+#disks=`sudo parted --list | awk -F ": |, |Disk | " '/Disk \// { print $2" "$3$4 }'`
+disks=`fdisk -l 2> /dev/null | grep 'Disk /' | sed 's/Disk \(.*\): \(.*\) \(.*\), .*/\1 \2\3/'`
 DSK=$(whiptail --nocancel --menu "Select the Disk to install to" 18 45 10 $disks 3>&1 1>&2 2>&3)
 
 drivelabel="multipass01"
-partboot="/dev/disk/by-label/$drivelabel"
+#partboot="/dev/disk/by-label/$drivelabel"
+partboot=${DSK}1
 tmpdir=/tmp/$drivelabel
 
 echo "## WILL COMPLETELY WIPE ${DSK}"
 read -p "Press [Enter] key to continue"
-sudo sgdisk --zap-all ${DSK}
+#sudo sgdisk --zap-all ${DSK}
 
 sudo parted -s ${DSK} mklabel msdos
 sudo parted -s ${DSK} -a optimal unit MB -- mkpart primary 1 -1
@@ -36,13 +38,14 @@ sudo mount $partboot $tmpdir
 if ( grep -q ${DSK} /etc/mtab ); then
 	echo "info: $partboot mounted at $tmpdir"
 
-	sudo grub-install --no-floppy --root-directory=$tmpdir ${DSK}
+	sudo grub-install --force --no-floppy --root-directory=$tmpdir ${DSK}
 
 	sleep 1
 
 	sudo chown -R `whoami` $tmpdir
 
-	cp /usr/lib/syslinux/bios/memdisk $tmpdir/boot/grub/
+	#cp /usr/lib/syslinux/bios/memdisk $tmpdir/boot/grub/
+	cp /boot/extlinux/memdisk $tmpdir/boot/grub/
 
 	pushd $tmpdir
 		curl -L https://github.com/Thermionix/multipass-usb/tarball/master | tar zx --strip 1
